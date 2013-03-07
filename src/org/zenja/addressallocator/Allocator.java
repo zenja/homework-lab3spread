2a10b1478d2a0432e216f32d72c17e600e0a9465
Xing, Wang <wang.xing.me@gmail.com>
2013-03-08 00:17:04 +0100

    delete no-needed files

--- src/org/zenja/addressallocator/Allocator.java
+++
@@ -1,7 +0,0 @@
-package org.zenja.addressallocator;
-
-import gmi.ExternalGMIListener;
-
-public interface Allocator extends ExternalGMIListener {
-
-}
--- src/org/zenja/addressallocator/InternalAllocator.java
+++
@@ -1,7 +0,0 @@
-package org.zenja.addressallocator;
-
-import gmi.InternalGMIListener;
-
-public interface InternalAllocator extends InternalGMIListener {
-
-}
